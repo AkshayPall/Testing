dd7205e87706310101738b194fabdd1bc439a356
Akshay Pall <akshaypall@Akshays-Mac.local>
2014-12-20 17:46:27 -0500

    1st revision of proj.

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var MailSent: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //if((enterMessageTextField.text = nil) && (enterNameTextField.text = nil)) messageLabel.hidden = true}
+        //else{
+            messageLabel.hidden = false
+            let stringSpace = "   - "
+            messageLabel.text = enterMessageTextField.text + stringSpace + enterNameTextField.text
+            messageLabel.textColor = UIColor.cyanColor()
+            enterMessageTextField.text = ""
+            enterNameTextField.text = ""
+            enterMessageTextField.resignFirstResponder()
+            MailSent.setTitle("Mail Sent", forState: UIControlState.Normal)
+          //}
+    }
 }
+
 
