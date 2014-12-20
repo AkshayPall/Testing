7de5cad5997a5ab834977fe304ce79aad15994de
Akshay Pall <akshaypall@Akshays-Mac.local>
2014-12-20 18:40:13 -0500

    added a comment to test commits

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         //if((enterMessageTextField.text = nil) && (enterNameTextField.text = nil)) messageLabel.hidden = true}
+        //added comment to test commitss
         //else{
             messageLabel.hidden = false
             let stringSpace = "   - "
